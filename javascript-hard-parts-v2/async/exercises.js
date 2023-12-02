dfffcee79d8872d5fb88c418aa63bb7fb3797b5b
Angel Ivanov <angel.iwanov@gmail.com>
2023-12-02 21:51:56 +0200

    asynchronous

---
+++ javascript-hard-parts-v2/async/exercises.js
@@ -0,0 +1,188 @@
+/* CHALLENGE 1 */
+
+function sayHowdy() {
+  console.log("Howdy");
+}
+
+function testMe() {
+  setTimeout(sayHowdy, 0);
+  console.log("Partnah");
+}
+// After thinking it through, uncomment the following line to check your guess!
+// testMe(); // what order should these log out? Howdy or Partnah first?
+
+/* CHALLENGE 2 */
+
+function delayedGreet() {
+  setTimeout(() => {
+    console.log("welcome");
+  }, 3000);
+  // ADD CODE HERE
+}
+// Uncomment the following line to check your work!
+// delayedGreet(); // should log (after 3 seconds): welcome
+
+/* CHALLENGE 3 */
+
+function helloGoodbye() {
+  console.log("hello");
+  setTimeout(function goodbye() {
+    console.log("goodbye");
+  }, 2000);
+  // ADD CODE HERE
+}
+// Uncomment the following line to check your work!
+// helloGoodbye(); // should log: hello // should also log (after 3 seconds): good bye
+
+/* CHALLENGE 4 */
+
+function brokenRecord() {
+  // ADD CODE HERE
+  setInterval(function sayHi() {
+    console.log("hi again");
+  }, 1000);
+}
+// Uncomment the following line to check your work!
+// brokenRecord(); // should log (every second): hi again
+
+/* CHALLENGE 5 */
+
+function limitedRepeat() {
+  // ADD CODE HERE
+  const timer = setInterval(function sayHi() {
+    console.log("hi for now");
+  }, 1000);
+
+  setTimeout(function stopTimer() {
+    console.log("stop");
+    clearInterval(timer);
+  }, 5000);
+}
+// Uncomment the following line to check your work!
+// limitedRepeat(); // should log (every second, for 5 seconds): hi for now
+
+/* CHALLENGE 6 */
+
+function everyXsecsForYsecs(func, interval, duration) {
+  // ADD CODE HERE
+  const timer = setInterval(func, interval);
+
+  setTimeout(function stopTimer() {
+    console.log("STOP!");
+    clearInterval(timer);
+  }, duration);
+}
+// Uncomment the following lines to check your work!
+// function theEnd() {
+//   console.log("This is the end!");
+// }
+// everyXsecsForYsecs(theEnd, 2000, 20000); // should invoke theEnd function every 2 seconds, for 20 seconds): This is the end!
+
+/* CHALLENGE 7 */
+
+function delayCounter(target, wait) {
+  let count = 1;
+
+  return function consoleLog() {
+    const timer = setInterval(() => {
+      if (count < target) {
+        console.log(count);
+        count++;
+      } else {
+        clearInterval(timer);
+      }
+    }, wait);
+  };
+}
+
+// UNCOMMENT THESE TO TEST YOUR WORK!
+// const countLogger = delayCounter(3, 1000);
+// countLogger();
+// After 1 second, log 1
+// After 2 seconds, log 2
+// After 3 seconds, log 3
+
+/* CHALLENGE 8 */
+
+function promised(val) {
+  // ADD CODE HERE
+  return new Promise((res, rej) => {
+    return setTimeout(() => {
+      res(val);
+    }, 2000);
+  });
+}
+
+// UNCOMMENT THESE TO TEST YOUR WORK!
+// const createPromise = promised("wait for it...");
+// createPromise.then((val) => console.log(val));
+// will log "wait for it..." to the console after 2 seconds
+
+/* CHALLENGE 9 */
+
+class SecondClock {
+  constructor(cb) {
+    // ADD CODE HERE
+    this.cb = cb;
+    this.seconds = 0;
+  }
+  // ADD METHODS HERE
+  start() {
+    this.id = setInterval(() => {
+      this.cb(++this.seconds % 60);
+    }, 1000);
+  }
+
+  reset() {
+    this.seconds = 0;
+    clearInterval(this.id);
+  }
+}
+
+// UNCOMMENT THESE TO TEST YOUR WORK!
+// const clock = new SecondClock((val) => {
+//   console.log(val);
+// });
+// console.log("Started Clock.");
+// clock.start();
+// setTimeout(() => {
+//   clock.reset();
+//   console.log("Stopped Clock after 6 seconds.");
+// }, 6000);
+
+/* CHALLENGE 10 */
+
+function debounce(callback, interval) {
+  // ADD CODE HERE
+  let duration = 0;
+  let id;
+
+  return function () {
+    if (duration <= 0) {
+      duration = interval;
+      clearInterval(id);
+      id = setInterval(() => {
+        console.log("running", duration);
+        duration -= 100;
+      }, 100);
+      return callback();
+    }
+  };
+}
+
+// UNCOMMENT THESE TO TEST YOUR WORK!
+
+// function giveHi() {
+//   return "hi";
+// }
+// const giveHiSometimes = debounce(giveHi, 3000);
+// console.log(giveHiSometimes()); // -> 'hi'
+// setTimeout(function () {
+//   console.log(giveHiSometimes());
+// }, 2000); // -> undefined
+// setTimeout(function () {
+//   console.log(giveHiSometimes());
+// }, 4000); // -> 'hi'
+// setTimeout(function () {
+//   console.log(giveHiSometimes());
+// }, 8000); // -> 'hi'
